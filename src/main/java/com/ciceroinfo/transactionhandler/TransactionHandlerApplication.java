02f3a83b542766d87aa4de6b0fdf67a54844e673
ciceroinfo <cicero.info@gmail.com>
2021-12-17 13:58:47 -0300

    first commit

---
+++ src/main/java/com/ciceroinfo/transactionhandler/TransactionHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.ciceroinfo.transactionhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionHandlerApplication.class, args);
+	}
+
+}
